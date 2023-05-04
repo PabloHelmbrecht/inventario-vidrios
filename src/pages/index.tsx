9218b21b1778194f1e7018b8eb7dd8af37f0c64d
Pablo Helmbrecht <pablohelmbrecht@gmail.com>
2023-05-04 11:58:30 +0000

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,6 @@
+//React
+import { Fragment, useState } from "react";
+
 //Next
 import { type NextPage } from "next";
 import Head from "next/head";
@@ -9,6 +12,9 @@ import {
   type GridRowsProp,
   type GridColDef,
 } from "@mui/x-data-grid";
+
+//Headless UI
+import { Dialog, Transition } from "@headlessui/react";
 
 const rows: GridRowsProp = [
   { id: 1, col1: "Hello", col2: "World" },
@@ -22,6 +28,7 @@ const columns: GridColDef[] = [
 ];
 
 const Home: NextPage = () => {
+  const [isGlassCreatorOpen, setIsGlassCreatorOpen] = useState<boolean>(false);
   return (
     <>
       <Head>
@@ -36,7 +43,12 @@ const Home: NextPage = () => {
           </h1>
           <div className="flex flex-col justify-center gap-4">
             <div className="flex w-full justify-end">
-              <button className=" rounded-md border border-transparent bg-sky-600 px-4 py-2 text-sm font-medium text-white hover:bg-sky-700">
+              <button
+                onClick={() => {
+                  setIsGlassCreatorOpen(true);
+                }}
+                className=" rounded-md border border-transparent bg-sky-600 px-4 py-2 text-sm font-medium text-white hover:bg-sky-700"
+              >
                 Cargar Vidrio
               </button>
             </div>
@@ -63,6 +75,65 @@ const Home: NextPage = () => {
           </div>
         </div>
       </main>
+
+      <Transition appear show={isGlassCreatorOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-10"
+          onClose={() => setIsGlassCreatorOpen(false)}
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-md bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    Payment successful
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <p className="text-sm text-gray-500">
+                      Your payment has been successfully submitted. We’ve sent
+                      you an email with all of the details of your order.
+                    </p>
+                  </div>
+
+                  <div className="mt-4">
+                    <button
+                      type="button"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={() => setIsGlassCreatorOpen(false)}
+                    >
+                      Got it, thanks!
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
     </>
   );
 };
