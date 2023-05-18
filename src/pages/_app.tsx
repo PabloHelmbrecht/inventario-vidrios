59064c4f3ae997ed657e5a74539d6b29ffe6436a
Pablo Helmbrecht <pablohelmbrecht@gmail.com>
2023-05-18 12:06:51 +0000

    Update 4 files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { type AppType } from 'next/app'
 import { type Session } from 'next-auth'
 import { SessionProvider } from 'next-auth/react'
+import { Analytics } from '@vercel/analytics/react'
 
 import '~/styles/globals.css'
 
@@ -11,6 +12,7 @@ const MyApp: AppType<{ session: Session | null }> = ({ Component, pageProps: { s
         <SessionProvider session={session}>
             <Nav />
             <Component {...pageProps} />
+            <Analytics />
         </SessionProvider>
     )
 }
