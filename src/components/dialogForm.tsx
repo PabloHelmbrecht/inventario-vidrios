328c0a3b042ac50cce98bcd8fc378f2cc17fcd70
Pablo Helmbrecht <pablohelmbrecht@gmail.com>
2023-05-18 11:13:20 +0000

    Update dialogForm.tsx

--- src/components/dialogForm.tsx
+++ src/components/dialogForm.tsx
@@ -1,11 +1,11 @@
 //React
-import React, { Fragment } from 'react'
+import React, { Fragment, createContext, useContext } from 'react'
 
 //Headless UI
 import { Dialog, Transition } from '@headlessui/react'
 
 //Final Form
-import { Form } from 'react-final-form'
+import { Form, type FormRenderProps } from 'react-final-form'
 import createDecorator, { type Calculation } from 'final-form-calculate'
 
 interface dialogFormProps {
@@ -21,6 +21,10 @@ interface dialogFormProps {
     onSubmit: (values: object) => void
     decorator?: Calculation[]
 }
+
+const DialogFormContext = createContext<FormRenderProps<object> | null>(null)
+
+export const useDialogFormContext = () => useContext<FormRenderProps<object> | null>(DialogFormContext)
 
 export default function DialogForm({
     title = 'Título del Form',
@@ -72,38 +76,40 @@ export default function DialogForm({
                                     decorators={decorator ? [createDecorator(...decorator)] : null}
                                     initialValues={{ ...initialValues }}
                                     render={(props) => (
-                                        <form>
-                                            <Dialog.Title
-                                                as="h3"
-                                                className={` ${
-                                                    titleStyles ?? ''
-                                                } text-lg font-medium leading-6 text-gray-900`}>
-                                                {title}
-                                            </Dialog.Title>
+                                        <DialogFormContext.Provider value={props}>
+                                            <form>
+                                                <Dialog.Title
+                                                    as="h3"
+                                                    className={` ${
+                                                        titleStyles ?? ''
+                                                    } text-lg font-medium leading-6 text-gray-900`}>
+                                                    {title}
+                                                </Dialog.Title>
 
-                                            <div className="mt-2 grid grid-cols-6 gap-4">{children}</div>
+                                                <div className="mt-2 grid grid-cols-6 gap-4">{children}</div>
 
-                                            <div className="mt-4">
-                                                <button
-                                                    type="button"
-                                                    className={` rounded-md border border-transparent ${buttonStyles} px-4 py-2 text-sm font-medium text-white disabled:bg-slate-400 `}
-                                                    onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
-                                                        if (e) {
-                                                            props
-                                                                .handleSubmit(e)
-                                                                ?.then((val) => val)
-                                                                .catch((e) => {
-                                                                    console.log(e)
-                                                                })
+                                                <div className="mt-4">
+                                                    <button
+                                                        type="button"
+                                                        className={` rounded-md border border-transparent ${buttonStyles} px-4 py-2 text-sm font-medium text-white disabled:bg-slate-400 `}
+                                                        onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+                                                            if (e) {
+                                                                props
+                                                                    .handleSubmit(e)
+                                                                    ?.then((val) => val)
+                                                                    .catch((e) => {
+                                                                        console.log(e)
+                                                                    })
 
-                                                            setIsOpen(false)
-                                                        }
-                                                    }}
-                                                    disabled={props.submitting || props.hasValidationErrors}>
-                                                    {buttonText}
-                                                </button>
-                                            </div>
-                                        </form>
+                                                                setIsOpen(false)
+                                                            }
+                                                        }}
+                                                        disabled={props.submitting || props.hasValidationErrors}>
+                                                        {buttonText}
+                                                    </button>
+                                                </div>
+                                            </form>
+                                        </DialogFormContext.Provider>
                                     )}
                                 />
                             </Dialog.Panel>
